77c032e008732e329e48b4d44635c0c01e41e56e
kpogodev <k.pogorzelski.dev@gmail.com>
2023-05-23 01:01:49 +0100

    Adding logo placeholder

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,10 @@
+import Logo from '../ui/Logo'
 import MenuDropdown from './MenuDropdown'
 
 function Navbar() {
   return (
     <div className='flex items-center justify-end bg-black p-3'>
+      <Logo />
       <MenuDropdown />
     </div>
   )
